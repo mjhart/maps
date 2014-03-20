c9f736bc74b2a8de6ad7b451a2685bd4d12de110
Matthew Hart <matthew_hart@brown.edu>
2014-03-20 16:30:10 -0400

    actually added pathfinder

---
+++ src/gui/PathFinder.java
@@ -0,0 +1,44 @@
+package gui;
+
+import java.io.IOException;
+
+import javax.swing.SwingUtilities;
+
+import tmp.Controller;
+
+public class PathFinder extends Thread {
+	
+	private String _src;
+	private String _dst; 
+	private AltDrawingPanel _dp;
+	Controller _c;
+	
+	
+	public PathFinder(String src, String dst, AltDrawingPanel dp, Controller c) {
+		_src = src;
+		_dst = dst;
+		_dp = dp;
+		_c = c;
+	}
+	
+	@Override
+	public void run() {
+		synchronized(_dp.getPath()) {
+			try {
+				_dp.getPath().removeAll(_dp.getPath());
+				_dp.getPath().addAll(_c.getPath(_src, _dst));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		SwingUtilities.invokeLater(paint);
+	}
+	
+	private Runnable paint = new Runnable() {
+		public void run() {
+			_dp.repaint();
+		}
+	};
+
+}
