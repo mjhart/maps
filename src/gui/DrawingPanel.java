d288d5ec07b0d0cb6e804b2bb6a40eefc2586004
Matthew Hart <matthew_hart@brown.edu>
2014-03-20 20:43:58 -0400

    got rid of load in dp

--- src/gui/DrawingPanel.java
+++ src/gui/DrawingPanel.java
@@ -3,13 +3,6 @@ package gui;
 import graph.*;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -42,22 +35,19 @@ public class DrawingPanel extends JPanel {
 	private int[] dMin;
 	
 	private Controller c;
+	int zoom = 10;
+	private InputPanel _ip;
 	
+	// path information
 	private Node _start;
 	private Node _end;
 	private List<Node> _path;
 	
+	// tile loading structures
 	private List<Tile> tiles;
 	private LinkedBlockingQueue<Tile> _tileQueue;
 	private TileLoader _tileLoader;
-	
-	public boolean load = false;
-	
-	int zoom = 10;
-	
-	private HashSet<Node> _nodes;
-	private HashSet<Edge> _ways;
-	private InputPanel _ip;
+
 	
 	public DrawingPanel(Controller c) {
 		super();
@@ -66,6 +56,7 @@ public class DrawingPanel extends JPanel {
 		this.setBackground(new Color(204,204,255));
 		//this.setOpaque(false);
 		
+		// define starting window
 		wMax = new double[2];
 		wMin = new double[2];
 		wMax[0] = -71.40;
@@ -78,13 +69,16 @@ public class DrawingPanel extends JPanel {
 		
 		this.c = c;
 		
+		// initialize tile loading
 		tiles = Collections.synchronizedList(new LinkedList<Tile>());
 		_tileQueue = new LinkedBlockingQueue<Tile>();
 		_tileLoader = new TileLoader(_tileQueue, tiles, c, this);
 		_tileLoader.start();
 		
+		// load first data set
 		loadData();
 		
+		// set up listeners
 		PanListener pan = new PanListener(this);
 		this.addMouseListener(pan);
 		this.addMouseMotionListener(pan);
@@ -94,13 +88,20 @@ public class DrawingPanel extends JPanel {
 		_ip = new InputPanel(this, c);
 	}
 	
+	/**
+	 * Calculates the tiles that are in the current window
+	 * plus a small buffer and adds them to the loading
+	 * queue
+	 */
 	public void loadData() {
 		
+		// calculate new data box
 		dMin[0] = lonToTx(wMin[0] - TILE_SIZE/2);
 		dMin[1] = latToTy(wMin[1] - TILE_SIZE/2);
 		dMax[0] = lonToTx(wMax[0] + 3*TILE_SIZE/2);
 		dMax[1] = latToTy(wMax[1] + 3*TILE_SIZE/2);
 		
+		// remove tiles outside data box
 		synchronized (tiles) {
 			List<Tile> toRemove = new LinkedList<Tile>();
 			for(int i=0; i<tiles.size(); i++) {
@@ -111,8 +112,8 @@ public class DrawingPanel extends JPanel {
 			tiles.removeAll(toRemove);
 		}
 		
-		
-		LinkedList<Tile> temp = new LinkedList<Tile>();
+		// add new tiles to tile list
+		LinkedList<Tile> toAdd = new LinkedList<Tile>();
 		for(int i=dMin[0]; i<dMax[0]; i++) {
 			for(int j=dMin[1]; j<dMax[1]; j++) {
 				
@@ -126,12 +127,12 @@ public class DrawingPanel extends JPanel {
 					}
 				}
 				if(!inCache) {
-					temp.add(new Tile(i, j, new HashSet<Node>(), new HashSet<Edge>()));
+					toAdd.add(new Tile(i, j, new HashSet<Node>(), new HashSet<Edge>()));
 				}
 			}
 		}
 		
-		for(Tile t : temp) {
+		for(Tile t : toAdd) {
 			tiles.add(t);
 			_tileQueue.add(t);
 		}
@@ -153,18 +154,18 @@ public class DrawingPanel extends JPanel {
 		return (double) (y*TILE_SIZE + LAT_INIT);
 	}
 	
-	public void paintComponent(Graphics g){
+	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
 		g.setColor(getBackground());
 		Graphics2D brush = (Graphics2D) g;
 		
-		if(load) {
-			if(wMax[0] > txToLon(dMax[0]) || wMax[1] > tyToLat(dMax[1]) || 
-					wMin[0] < txToLon(dMin[0]) || wMin[1] < tyToLat(dMin[1])) {
-				loadData();
-			}
+		// if window intersects data box load data
+		if(wMax[0] > txToLon(dMax[0]) || wMax[1] > tyToLat(dMax[1]) || 
+				wMin[0] < txToLon(dMin[0]) || wMin[1] < tyToLat(dMin[1])) {
+			loadData();
 		}
 		
+		// clear old window and paint background
 		brush.clearRect(0, 0, this.getWidth(), this.getHeight());
 		brush.setColor(new Color(204, 204, 255));
 		brush.fillRect(0, 0, this.getWidth(), this.getHeight());
@@ -172,10 +173,9 @@ public class DrawingPanel extends JPanel {
 		//Stroke wayStroke = new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL);
 		//Stroke nodeStroke = new BasicStroke(3);
 		
-		synchronized (tiles) {
-			for(Tile t : tiles) {
-
-				if(t.isLoaded()) {
+		synchronized (tiles) { // get lock on tile list
+			for(Tile t : tiles) { 
+				if(t.isLoaded()) { // if tile has been loaded
 					
 					
 					/*   Debugging info
@@ -195,37 +195,47 @@ public class DrawingPanel extends JPanel {
 					*/
 					
 					brush.setColor(java.awt.Color.BLACK);
-					for(Node n : t.nodes){
+					
+					// draw nodes in tile
+					for(Node n : t.nodes){ 
 						paintNode(brush, n);
 					}
-
+					
+					// draw ways in tile
 					for(Edge e : t.ways){
 						paintWay(brush, e);
 					}
 				}
 			}
 		}
+		
+		// draw start point
 		if(_start != null) {
 			brush.setColor(java.awt.Color.GREEN);
 			brush.fillOval(lonToX(_start.getLon())-2, latToY(_start.getLat())-2, 5, 5);
 		}
+		
+		// draw end point
 		if(_end!=null){
 			brush.setColor(java.awt.Color.RED);
 			brush.fillOval(lonToX(_end.getLon())-2, latToY(_end.getLat())-2, 5, 5);
 		}
 		
+		// draw path if
 		synchronized(_path) {
-			if(_path!=null){
-				brush.setColor(java.awt.Color.GREEN);
-				for(int i = 1; i < _path.size(); i++){
-					brush.drawLine(lonToX(_path.get(i-1).getLon()), latToY(_path.get(i-1).getLat()), lonToX(_path.get(i).getLon()), latToY(_path.get(i).getLat()));
-				}
+			brush.setColor(java.awt.Color.GREEN);
+			for(int i = 1; i < _path.size(); i++){
+				brush.drawLine(lonToX(_path.get(i-1).getLon()), latToY(_path.get(i-1).getLat()), lonToX(_path.get(i).getLon()), latToY(_path.get(i).getLat()));
 			}
 		}
 		
 	}
 	
-	public void updateZoom(int zoom){ 
+	/**
+	 * Updates the zoom level
+	 * @param zoom
+	 */
+	private void updateZoom(int zoom){ 
 		
 		if(zoom > 0) {
 
@@ -247,6 +257,10 @@ public class DrawingPanel extends JPanel {
 		updateZoom(this.zoom+1);
 	}
 	
+	/**
+	 * Moves the window delta units 
+	 * @param delta
+	 */
 	public void moveWindow(double[] delta) {
 		
 		double dx = delta[0]*(wMax[0]-wMin[0])/this.getWidth();
@@ -276,12 +290,22 @@ public class DrawingPanel extends JPanel {
 		return (double) (wMax[1] - y*(wMax[1]-wMin[1])/this.getHeight());
 	}
 	
+	/**
+	 * Paints the given node
+	 * @param brush
+	 * @param node
+	 */
 	private void paintNode(Graphics2D brush, Node node) {
 		int x = lonToX(node.getLon());
 		int y = latToY(node.getLat());
 		brush.drawRect(x,y,1,1);
 	}
 	
+	/**
+	 * Paints the given way
+	 * @param brush
+	 * @param way
+	 */
 	private void paintWay(Graphics2D brush, Edge way) {
 		int x1 = lonToX(way.getSource().getLon());
 		int y1 = latToY(way.getSource().getLat());
@@ -290,6 +314,13 @@ public class DrawingPanel extends JPanel {
 		brush.drawLine(x1, y1, x2, y2);
 	}
 	
+	/**
+	 * Sets the start and end points to the nearest 
+	 * neighbor of a click
+	 * 
+	 * @param x
+	 * @param y
+	 */
 	public void clickAt(int x, int y) {
 		double[] coords = {xToLon(x), yToLat(y)};
 		if(_start==null){
@@ -302,6 +333,11 @@ public class DrawingPanel extends JPanel {
 		this.repaint();
 	}
 
+	/**
+	 * Starts a new Astar search from src to dst in a new thread
+	 * @param src
+	 * @param dst
+	 */
 	public void startSearch(Node src, Node dst) {
 		PathFinder pf = new PathFinder(src.toString(), dst.toString(), this, c, _ip);
 		pf.start();
